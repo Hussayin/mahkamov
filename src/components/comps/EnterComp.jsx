544178bf1c5075079c3495b8e976366cde186f19
Husayin Bahramov <husayinbahramov@gmial.com>
2026-01-28 19:22:07 +0500

    all is true

--- src/components/comps/EnterComp.jsx
+++ src/components/comps/EnterComp.jsx
@@ -23,7 +23,7 @@ const logoss = [
 
 const EnterComp = () => {
   return (
-    <div className="bg-[#101625] h-auto mt-[65px] flex items-center">
+    <div className="bg-[#101625] h-auto mt-[55px] flex items-center">
       {/* ASOSIY CONTAINER */}
       <div className="w-full mb-[40px] flex flex-col gap-[25px]">
         {/* tanishtiruv */}
